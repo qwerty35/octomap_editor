821c2c59e1a7e00bcb4dfcc33af1da1138322fc0
Jungwon Park <wjstk335@gmail.com>
2022-03-24 15:50:46 +0900

    better input output

--- octomap_editor/src/octomap_editor_node.cpp
+++ octomap_editor/src/octomap_editor_node.cpp
@@ -1,5 +1,6 @@
 
 #include <ros/ros.h>
+#include <ros/package.h>
 
 #include <interactive_markers/interactive_marker_server.h>
 #include <interactive_markers/menu_handler.h>
@@ -18,11 +19,13 @@
 
 using namespace visualization_msgs;
 
-ros::Publisher selection_box_publisher;
 ros::ServiceClient delete_box_client;
+ros::ServiceClient save_octomap_client;
 ros::Publisher add_point_cloud_publisher;
 octomap::point3d selection_point1;
 octomap::point3d selection_point2;
+std::string save_file_name;
+double resolution;
 
 // %Tag(vars)%
 boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server;
@@ -32,7 +35,6 @@ interactive_markers::MenuHandler menu_handler;
 
 void selectionBoxCallback(const ros::TimerEvent&)
 {
-
     static tf::TransformBroadcaster br;
     tf::Transform transform;
     transform.setOrigin( tf::Vector3(selection_point1.x(), selection_point1.y(), selection_point1.z()) );
@@ -44,7 +46,6 @@ void selectionBoxCallback(const ros::TimerEvent&)
     visualization_msgs::MarkerArray marker_array;
     octomap::point3d selection_box_pose = (selection_point1 + selection_point2) * 0.5;
     octomap::point3d selection_box_delta = selection_point1 - selection_point2;
-
     visualization_msgs::Marker marker;
     marker.header.frame_id = "base_link";
     marker.id = 0;
@@ -68,31 +69,42 @@ void selectionBoxCallback(const ros::TimerEvent&)
 
     marker_array.markers.emplace_back(marker);
 
-    selection_box_publisher.publish(marker_array);
+//    selection_box_publisher.publish(marker_array);
+}
+
+void tfCallback(const ros::TimerEvent&)
+{
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    transform.setOrigin( tf::Vector3(selection_point1.x(), selection_point1.y(), selection_point1.z()) );
+    tf::Quaternion q;
+    q.setRPY(0, 0, 0);
+    transform.setRotation(q);
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "selection_point1"));
 }
 
 // %Tag(Box)%
-Marker makeBox( InteractiveMarker &msg )
+Marker makeBox( InteractiveMarker &msg, double a )
 {
     Marker marker;
 
     marker.type = Marker::CUBE;
-    marker.scale.x = msg.scale * 0.45;
-    marker.scale.y = msg.scale * 0.45;
-    marker.scale.z = msg.scale * 0.45;
-    marker.color.r = 0.5;
-    marker.color.g = 0.5;
-    marker.color.b = 0.5;
-    marker.color.a = 0.0;
+    marker.scale.x = msg.scale * 0.5;
+    marker.scale.y = msg.scale * 0.5;
+    marker.scale.z = msg.scale * 0.5;
+    marker.color.r = 0.0;
+    marker.color.g = 1.0;
+    marker.color.b = 0.0;
+    marker.color.a = a;
 
     return marker;
 }
 
-InteractiveMarkerControl& makeBoxControl( InteractiveMarker &msg )
+InteractiveMarkerControl& makeBoxControl( InteractiveMarker &msg, double a )
 {
     InteractiveMarkerControl control;
     control.always_visible = true;
-    control.markers.push_back( makeBox(msg) );
+    control.markers.push_back( makeBox(msg, a) );
     msg.controls.push_back( control );
 
     return msg.controls.back();
@@ -102,9 +114,9 @@ InteractiveMarkerControl& makeBoxControl( InteractiveMarker &msg )
 // %Tag(processFeedback)%
 void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
 {
-    std::ostringstream s;
-    s << "Feedback from marker '" << feedback->marker_name << "' "
-      << " / control '" << feedback->control_name << "'";
+//    std::ostringstream s;
+//    s << "Feedback from marker '" << feedback->marker_name << "' "
+//      << " / control '" << feedback->control_name << "'";
 
     std::ostringstream mouse_point_ss;
     if( feedback->mouse_point_valid )
@@ -122,7 +134,6 @@ void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPt
             break;
 
         case visualization_msgs::InteractiveMarkerFeedback::MENU_SELECT:
-            ROS_INFO_STREAM( s.str() << ": menu item " << feedback->menu_entry_id << " clicked" << mouse_point_ss.str() << "." );
             if(feedback->menu_entry_id == 1){
                 ROS_INFO_STREAM("Delete markers in selection box");
                 geometry_msgs::Point min, max;
@@ -152,8 +163,7 @@ void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPt
                 max.y = std::max(selection_point1.y(), selection_point2.y());
                 max.z = std::max(selection_point1.z(), selection_point2.z());
 
-                float res = 0.1; //TODO: param
-
+                float res = resolution;
                 for(float x = min.x; x < max.x; x += res){
                     for(float y = min.y; y < max.y; y += res){
                         for(float z = min.z; z < max.z; z += res){
@@ -175,6 +185,11 @@ void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPt
                     rate.sleep();
                 }
             }
+            else if(feedback->menu_entry_id == 3){
+                ROS_INFO_STREAM("Save octomap");
+                std::string command = "rosrun octomap_server octomap_saver " + save_file_name;
+                system(command.c_str());
+            }
             break;
 
         case visualization_msgs::InteractiveMarkerFeedback::POSE_UPDATE:
@@ -188,6 +203,21 @@ void processFeedback( const visualization_msgs::InteractiveMarkerFeedbackConstPt
                                                     feedback->pose.position.y,
                                                     feedback->pose.position.z);
             }
+
+            {
+                visualization_msgs::InteractiveMarker menu_marker;
+                server->get("selected_region", menu_marker );
+                octomap::point3d selection_box_pose = (selection_point1 + selection_point2) * 0.5;
+                octomap::point3d selection_box_delta = selection_point1 - selection_point2;
+                menu_marker.pose.position.x = selection_box_pose.x();
+                menu_marker.pose.position.y = selection_box_pose.y();
+                menu_marker.pose.position.z = selection_box_pose.z();
+                menu_marker.controls[0].markers[0].scale.x = abs(selection_box_delta.x());
+                menu_marker.controls[0].markers[0].scale.y = abs(selection_box_delta.y());
+                menu_marker.controls[0].markers[0].scale.z = abs(selection_box_delta.z());
+                server->insert(menu_marker);
+            }
+
             break;
 
         case visualization_msgs::InteractiveMarkerFeedback::MOUSE_DOWN:
@@ -219,7 +249,7 @@ void make6DofMarker(const tf::Vector3& position, const std::string& marker_name)
     int_marker.description = marker_name;
 
     // insert a box
-    makeBoxControl(int_marker);
+    makeBoxControl(int_marker, 0);
     int_marker.controls[0].interaction_mode = interaction_mode;
 
     InteractiveMarkerControl control;
@@ -237,7 +267,7 @@ void make6DofMarker(const tf::Vector3& position, const std::string& marker_name)
     control.orientation.x = 0;
     control.orientation.y = 1;
     control.orientation.z = 0;
-    control.name = "move_z";
+    control.name = "move_y";
     control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
     int_marker.controls.push_back(control);
 
@@ -245,7 +275,7 @@ void make6DofMarker(const tf::Vector3& position, const std::string& marker_name)
     control.orientation.x = 0;
     control.orientation.y = 0;
     control.orientation.z = 1;
-    control.name = "move_y";
+    control.name = "move_z";
     control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
     int_marker.controls.push_back(control);
 
@@ -255,6 +285,30 @@ void make6DofMarker(const tf::Vector3& position, const std::string& marker_name)
     menu_handler.apply( *server, int_marker.name );
 }
 // %EndTag(6DOF)%
+
+void makeMenuMarker(const tf::Vector3& position, const std::string& marker_name)
+{
+    unsigned int interaction_mode = visualization_msgs::InteractiveMarkerControl::MENU;
+
+    InteractiveMarker int_marker;
+    int_marker.header.frame_id = "base_link";
+    tf::pointTFToMsg(position, int_marker.pose.position);
+    int_marker.scale = 2.0;
+
+    int_marker.name = marker_name;
+    int_marker.description = marker_name;
+
+    // insert a box
+    makeBoxControl(int_marker, 0.3);
+    int_marker.controls[0].interaction_mode = interaction_mode;
+
+    InteractiveMarkerControl control;
+    control.orientation_mode = InteractiveMarkerControl::FIXED;
+
+    server->insert(int_marker);
+    server->setCallback(int_marker.name, &processFeedback);
+    menu_handler.apply( *server, int_marker.name );
+}
 
 // %Tag(Moving)%
 void makeMovingMarker( const tf::Vector3& position )
@@ -278,7 +332,7 @@ void makeMovingMarker( const tf::Vector3& position )
 
     control.interaction_mode = InteractiveMarkerControl::MOVE_PLANE;
     control.always_visible = true;
-    control.markers.push_back( makeBox(int_marker) );
+    control.markers.push_back( makeBox(int_marker, 0.3) );
     int_marker.controls.push_back(control);
 
     server->insert(int_marker);
@@ -290,13 +344,16 @@ void makeMovingMarker( const tf::Vector3& position )
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "basic_controls");
-    ros::NodeHandle n;
+    ros::NodeHandle n("~");
 
-    selection_box_publisher = n.advertise<visualization_msgs::MarkerArray>("/selection_box", 1);
-    ros::Timer selection_box_timer = n.createTimer(ros::Duration(0.01), selectionBoxCallback);
-
+    ros::Timer tf_broadcaster_timer = n.createTimer(ros::Duration(0.01), tfCallback);
     delete_box_client = n.serviceClient<octomap_msgs::BoundingBoxQuery>("/octomap_server/clear_bbx");
     add_point_cloud_publisher = n.advertise<sensor_msgs::PointCloud2>("/cloud_in", 1);
+
+    std::string package_path = ros::package::getPath("octomap_editor");
+    n.param<std::string>("save_file_name", save_file_name, "default.bt");
+    n.param<double>("resolution", resolution, 0.1);
+    save_file_name = package_path + "/world/" + save_file_name;
 
     server.reset( new interactive_markers::InteractiveMarkerServer("basic_controls","",false) );
 
@@ -304,6 +361,7 @@ int main(int argc, char** argv)
 
     menu_handler.insert( "Delete markers in selection box", &processFeedback );
     menu_handler.insert( "Add markers in selection box", &processFeedback );
+    menu_handler.insert( "Save octomap", &processFeedback );
 
     tf::Vector3 position;
     position = tf::Vector3(0, 0, 0);
@@ -312,6 +370,8 @@ int main(int argc, char** argv)
     position = tf::Vector3(1, 1, 1);
     selection_point2 = octomap::point3d(1, 1, 1);
     make6DofMarker(position, "selection_point2");
+    position = tf::Vector3(0.5, 0.5, 0.5);
+    makeMenuMarker(position, "selected_region");
 
     server->applyChanges();
 
